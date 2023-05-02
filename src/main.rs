6558e37fbf0664a5a8fd0bce00e54ec70e5bd1c9
Tomek Kaczmarzyk <tomaszdawidkaczmarzyk@gmail.com>
2023-05-02 02:08:56 +0200

    implementing wol logic

--- src/main.rs
+++ src/main.rs
@@ -1,36 +1,56 @@
-use serde::{Deserialize, Serialize};
+use std::error::Error;
 use warp::{Filter, Rejection, Reply};
+use wake_on_lan;
 
+#[derive(Debug)]
+enum ApiError {
+    ComputerNotFound,
+    ErrorWhileSendingPacket,
+}
 
-#[derive(Debug, Serialize, Deserialize, Clone)]
+type Computers = Vec<Computer>;
+impl warp::reject::Reject for ApiError {}
+
+#[derive(Debug, Clone)]
 struct Computer {
     id: i32,
     mac_address: String,
 }
 
-type Computers = Vec<Computer>;
-
-#[derive(Debug)]
-enum ApiError {
-    UserNotFound,
-}
-
-impl warp::reject::Reject for ApiError {}
-
 async fn get_computer_by_id(id: i32, computers: Computers) -> Result<impl Reply, Rejection> {
     if computers.iter().any(|c| c.id == id) {
         match computers.into_iter().nth(id as usize) {
             Some(comp) => {
-                Ok(warp::reply::json(&comp.mac_address))
+                match wake_computer(comp.mac_address.clone()) {
+                    Ok(_) => {
+                        println!("Packet sent successfully.");
+                        Ok(warp::reply::json(&true))
+                    },
+                    Err(error) => {
+                        println!("Error sending packet: {}", error);
+                        Err(warp::reject::custom(ApiError::ErrorWhileSendingPacket))
+                    },
+                }
             }
-            None => {
-                Err(warp::reject::custom(ApiError::UserNotFound))
-            }
+            None => Err(warp::reject::custom(ApiError::ComputerNotFound))
         }
 
         
     } else {
-        Err(warp::reject::custom(ApiError::UserNotFound))
+        Err(warp::reject::custom(ApiError::ComputerNotFound))
+    }
+}
+
+fn wake_computer(mac_address: String) -> Result<bool, Box<dyn Error>> {
+    let converted_mac_address: [u8; 6] = match mac_address.as_bytes().try_into() {
+        Ok(array) => array,
+        Err(error) => return Err(format!("Error during converting into byte array, {}", error).into()),
+    };
+
+    let magic_packet = wake_on_lan::MagicPacket::new(&converted_mac_address);
+    match magic_packet.send() {
+        Ok(()) => Ok(true),
+        Err(error) => return Err(format!("Error sending packet, {}", error).into()),
     }
 }
 
@@ -59,7 +79,7 @@ async fn main() {
     let computers: Computers = get_computers();
     let computers_filter = warp::any().map(move || computers.clone());
 
-    let wake_computer_by_id = warp::path!("computer" / i32)
+    let wake_computer_by_id = warp::path!("wol_computer" / i32)
         .and(warp::get())
         .and(computers_filter.clone())
         .and_then(get_computer_by_id);
